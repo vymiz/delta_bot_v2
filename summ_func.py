c28c5c692b92370fbce3a15e9124c5d518e3c431
vymiz <vymiz020777@gmail.com>
2022-01-27 22:34:48 +0300

    summ function is added

---
+++ summ_func.py
@@ -0,0 +1,23 @@
+from adresses import *
+
+def summa():
+    file = open(out, 'r')
+    f = file.readlines()
+    file.close()
+
+    posa = []
+    price = []
+    total = []
+
+    for i in f:
+        i = i.strip().split(',')
+        posa.append(int(i[0]))
+        price.append(int(i[1]))
+
+    posa.append(-sum(posa))
+    price.append(price[-1])
+
+    for i in range(len(posa)):
+        total.append(posa[i] * price[i])
+
+    return sum(total)
