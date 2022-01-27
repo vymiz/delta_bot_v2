a170b7f824e34db16a3a2f5b761ef02f98dc16d7
vymiz <vymiz020777@gmail.com>
2022-01-27 22:32:32 +0300

    telegramm module is added

---
+++ tb.py
@@ -0,0 +1,20 @@
+import requests
+
+api_token = '1862933682:AAE4bucIW6lD4-pX_Qmr-wZFJSuNE1j-qZg'
+
+def send():
+   requests.get('https://api.telegram.org/bot{}/sendMessage'.format(api_token), params=dict(
+   chat_id=456899553,
+   text='Connect Error!'
+   ))
+
+'''
+если бот не работает, то через BotFather нодо получить новый токен
+https://api.telegram.org/bot1862933682:AAE4bucIW6lD4-pX_Qmr-wZFJSuNE1j-qZg/getUpdates
+
+1862933682:AAE4bucIW6lD4-pX_Qmr-wZFJSuNE1j-qZg
+
+456899553
+'''
+
+send()
