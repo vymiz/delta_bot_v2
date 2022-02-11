c260cdf6310f25120eac90905232ddb8676b3d82
VyMiZ <vymiz020777@gmail.com>
2022-02-11 17:14:47 +0300

    pre-commit

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from tb import *
 from write_func import *
 from strike_func import *
 from erase_func import *
-# from math import copysign
 
 size = 1  # pose size
 pause = 60
@@ -14,38 +13,33 @@ trigger = 0
 
 erase()
 
-delta, strike, price = read()
+delta_raw, strike, price = read()
 
-d, strike_new = get_strike(delta, strike, price)
+delta, strike_new = get_strike(delta_raw, strike, price)
 strike_old = strike_new
-new_pose_size = size * d
+new_pose_size = size * delta
 old_pose_size = new_pose_size
 price_old = price
 counter = 0
 
 while True:
     try:
-        delta, strike, price = read()
+        delta_raw, strike, price = read()
     except:
         time.sleep(1)
-        delta, strike, price = read()
+        delta_raw, strike, price = read()
 
-    d, strike_new = get_strike(delta, strike, price)
+    delta, strike_new = get_strike(delta_raw, strike, price)
 
     # if strikes are not equal we interrupt this WHILE iteration
     if strike_new != strike_old:
         strike_old = strike_new
-        new_pose_size = old_pose_size = size * d
+        new_pose_size = old_pose_size = size * delta
         print('strike change')
         f_write(summa(tmbl=True), price)
-        # if counter < 2:
-        #     print(posa, price)
-        # else:
-        #     print(posa, price, summa())
-        # print('*' * 10)
         continue
 
-    new_pose_size = size * d
+    new_pose_size = size * delta
 
     # if prices are the same we increase trigger by 1
     if price == price_old:
@@ -56,9 +50,14 @@ while True:
         trigger = 0
 
     if new_pose_size != old_pose_size:
+        if not ((new_pose_size < 0 and old_pose_size > 0) or (new_pose_size > 0 and old_pose_size < 0)):
+            if counter > 2:
+                print('trend change')
+                print(summa(tmbl=True), price, summa())
+                f_write(summa(tmbl=True), price)
+
         large_posa = new_pose_size - old_pose_size
         old_pose_size = new_pose_size
-        # posa = int(copysign(1, large_posa)) #  reduce pose size to 1 with the same SIGN
         posa = large_posa
         counter += 1
         f_write(posa, price)
